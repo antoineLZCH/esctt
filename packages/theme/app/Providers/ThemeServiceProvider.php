960e53a2645b371c4875e09e3c7def3123cd0dbd
Antoine LOZACH <lzch.antoine@gmail.com>
2026-09-06 22:52:38 +0200

    refactor: remove empty Sage provider overrides

--- packages/theme/app/Providers/ThemeServiceProvider.php
+++ packages/theme/app/Providers/ThemeServiceProvider.php
@@ -6,23 +6,4 @@ use Roots\Acorn\Sage\SageServiceProvider;
 
 class ThemeServiceProvider extends SageServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        parent::register();
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
-    }
 }
