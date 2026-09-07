33e7b95c66f41230a0dca8991bf738a408517c54
Antoine LOZACH <lzch.antoine@gmail.com>
2026-09-07 05:53:15 +0200

    chore: remove temporary coverage diagnostic
    Restore the repository coverage command after verifying the branch paths.\n\nRefs #27

--- scripts/ci/php-coverage.sh
+++ scripts/ci/php-coverage.sh
@@ -9,7 +9,6 @@ cd "$root_dir/apps/wordpress"
 ESCTT_WORDPRESS_TESTS=1 XDEBUG_MODE=coverage vendor/bin/pest \
     --coverage \
     --path-coverage \
-    --coverage-html="$coverage_dir/html" \
     --min=100 \
     --coverage-clover="$coverage_dir/clover.xml"
 
