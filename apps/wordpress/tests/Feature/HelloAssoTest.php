9d7e7a63827820bf19d918860d43e4b6a53d9018
Antoine LOZACH <lzch.antoine@gmail.com>
2026-09-08 08:49:06 +0200

    test(helloasso): cover invalid widget parse

--- apps/wordpress/tests/Feature/HelloAssoTest.php
+++ apps/wordpress/tests/Feature/HelloAssoTest.php
@@ -60,7 +60,7 @@ test('the HelloAsso block validates distinct membership and widget URLs', functi
         ->and(\App\helloasso_membership_url('https://example.com/associations/example/adhesions/adhesion-2026'))->toBeNull()
         ->and(\App\helloasso_membership_url('https://www.helloasso.com/not-a-membership'))->toBeNull()
         ->and(\App\helloasso_membership_url('javascript:alert(1)'))->toBeNull()
-        ->and(\App\helloasso_widget_url('http://www.helloasso.com/associations/example/adhesions/adhesion-2026/widget'))->toBeNull()
+        ->and(\App\helloasso_widget_url('http://'))->toBeNull()
         ->and(\App\helloasso_widget_url('https://user:pass@www.helloasso.com/associations/example/adhesions/adhesion-2026/widget'))->toBeNull()
         ->and(\App\helloasso_widget_url('https://[invalid'))->toBeNull()
         ->and(\App\helloasso_widget_url('https://www.helloasso.com/associations/example/adhesions/adhesion-2026/widget#fragment'))->toBeNull()
